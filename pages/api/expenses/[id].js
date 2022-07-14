e32975052b0734e14be51f9a47736ce0e02c234f
Kumail Khan <hassankumail9@gmail.com>
2022-07-15 01:38:16 +0200

    PUT and DEL endpoints, without auth

--- pages/api/expenses/[id].js
+++ pages/api/expenses/[id].js
@@ -10,10 +10,33 @@ export default async function handler(req, res) {
         if (!expense) {
             return res.status(404).json({ message: 'Not Found' })
         }
-        res.status(200).json(expense )
+        res.status(200).json(expense)
     }
     if (req.method === 'PUT') {
+        const { trip, name, date, amount, currency } = req.body
+
+        await prisma.expense.update({
+            data: {
+                trip,
+                name,
+                date,
+                amount,
+                currency,
+            },
+            where: {
+                id: parseInt(req.query.id),
+            },
+        })
+
+        return res.status(200).end()
     }
     if (req.method === 'DELETE') {
+        await prisma.expense.delete({
+            where: {
+                id: parseInt(req.query.id),
+            },
+        })
+
+        return res.status(200).end()
     }
 }
